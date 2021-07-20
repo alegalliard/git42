60787535c4d27cd1056b005cb95966fe5c82fb1d
adias-de <adias-de@student.42sp.org.br>
2021-07-20 20:39:48 +0000

    minha pasta de login adias-de

---
+++ adias-de/main.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adias-de <adias-de@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/20 20:37:41 by adias-de          #+#    #+#             */
+/*   Updated: 2021/07/20 20:39:15 by adias-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int main(void)
+{
+	//Coisas que você sempre quis dizer, mas nunca teve coragem:
+	// Norminette chatona
+	return(0);
+}
