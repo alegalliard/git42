95245ce32ce57158b838f498552bfd12e8dbec5f
adias-de <adias-de@student.42sp.org.br>
2021-07-23 14:50:31 +0000

    exercícios do Shell00 ao C00

--- pointers/pointers-array.c
+++ pointers/pointers-array.c
@@ -21,6 +21,7 @@ int main(void)
 	tab0[2] = 145;
 	tab02[1] = tab0;
 	// tab02[1][2] = 18; //se descomentar essa linha, ele vai sobrescrever o 145 pra 18
+	// cáculos aritiméticos para pointers e deference
 	// *(tab02[1] + 2) = 18;
 	// *(*(tab02 + 1) + 2) = 19; //se descomentar essa linha, ele vai sobrescrever o 145 pra 19. É quivalente a fazer tab0[2] 
 	printf("%d\n", *(tab0 + 2)); //print 145, porque faz o equivalente a tab0[2]
---
+++ pointers/strings.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/*
+strings não existem para o computador
+é um conjuntos de bytes que termina em um byte 0 (\0)
+*/
+int main(void)
+{
+
+}
