9edfcf040bf5d1eec5c79895a8fe4c7a3b6c1a6f
adias-de <adias-de@student.42sp.org.br>
2021-07-23 13:47:32 +0000

    pointers em C

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
@@ -9,14 +10,43 @@ int main(void)
 	int 	**ptr3;
 
 	ptr = &a;
-	printf("%p\n", ptr); //mostra no endereço da memória do a
+	// printf("%p\n", ptr); //mostra no endereço da memória do a
 	
 	ptr = &b;
-	printf("%p\n", &b); //endereço da memória do b
-	printf("%p\n", ptr); //endereço da memória virtual que aponta pro b
+	// printf("%p\n", &b); //endereço da memória do b
+	// printf("%p\n", ptr); //endereço da memória virtual que aponta pro b
 
 	ptr2 = ptr;
 	ptr3 = &ptr2;
-	printf("%p\n", ptr3); //pointer para o pointer
+	// printf("%p\n", ptr3); //pointer para o pointer
+
+
+	int 	c;
+	int 	*ptr01;
+	int 	**ptr02;
+	int 	***ptr03;
+	int 	****ptr04;
+	int 	*****ptr05;
+	int 	******ptr06;
+
+	c = 42;
+	ptr01 = &c;
+	ptr02 = &ptr01;
+	ptr03 = &ptr02;
+	ptr04 = &ptr03;
+	ptr05 = &ptr04;
+	ptr06 = &ptr05;
+
+	printf("%d\n", ******ptr06); //retorna o valor da variável
+	printf("%p\n", &ptr06); //retorna o endereço de memória
+
+	// write(1, &ptr06, 1); //retorna 8
+	// write(1, &ptr06, 2); //retorna ��
+	// write(1, ******&ptr06, 1); //retorna *
+	// write(1, ******&ptr06, 2); //retorna *
+	// write(1, ******&ptr06+'0', 2); //retorna �
+	// write(1, ptr06+'0', 2); //retorna �'
+	// write(1, ptr06, 2); //retorna 0
+
 	return(0);
 }
