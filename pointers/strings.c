35521851ad328f1bd6a5ab6f1bf401d944ef7a7a
adias-de <adias-de@student.42sp.org.br>
2021-07-23 15:03:07 +0000

    comportamento de strings, exercicios

--- pointers/strings.c
+++ pointers/strings.c
@@ -3,9 +3,28 @@
 
 /*
 strings não existem para o computador
-é um conjuntos de bytes que termina em um byte 0 (\0)
+é um conjunto de bytes encadeados que termina em um byte 0 (\0)
+Ex: alo\0
 */
 int main(void)
 {
+	char *str;
+	str = "lol";/*l na tabela ASCII, o na tabela ASCII, l na tabela ASCII e \0
+	são 4 caracteres na memória
+	escrever em aspas duplas significa escrever em constantes, um lugar paticular para indicar
+	que algo foi escrito entre aspas e não pode mudar seu valor. Esse endereço não pode ser reescrito
+	*/
 
+	//str[0] = 'p'; vai dar erro 
+		//%c = char 
+		//%s = string da lin do stdio
+	printf("%c %s\n", *str, str); //l lol
+								  //*str é o endereço do primeiro caracter
+
+	/* Pra poder reescrever uma string é necessário passar um array de caracteres*/
+	char str_reescrevivel[] = "lol";
+	str_reescrevivel[0] = 'p';
+	printf("%c %s\n", *str_reescrevivel, str_reescrevivel); //agora funciona pol
+
+	return(0);
 }
